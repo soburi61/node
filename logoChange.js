22347a749988f56bf35493552ed8addb6be06b0b
soburi59 <591959ant@gmail.com>
2023-04-24 16:49:55 +0900

    update twitter

--- logoChange.js
+++ logoChange.js
@@ -3,17 +3,17 @@ let myImage = document.querySelector('.logo');
 
 myImage.addEventListener('mouseover',function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/logo.png') {
-      myImage.setAttribute('src','images/logo-hover.png');
+    if(mySrc === '/images/logo.png') {
+      myImage.setAttribute('src','/images/logo-hover.png');
     } else {
-      myImage.setAttribute('src','images/logo.png');
+      myImage.setAttribute('src','/images/logo.png');
     }
 });
 myImage.addEventListener('mouseleave',function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/logo.png') {
-      myImage.setAttribute('src','images/logo-hover.png');
+    if(mySrc === '/images/logo.png') {
+      myImage.setAttribute('src','/images/logo-hover.png');
     } else {
-      myImage.setAttribute('src','images/logo.png');
+      myImage.setAttribute('src','/images/logo.png');
     }
 });
