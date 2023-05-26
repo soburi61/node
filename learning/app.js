e52d5f8ae1b875b2f967afc7fb51d976d7bfb0ff
soburi59 <591959ant@gmail.com>
2023-05-26 14:40:27 +0900

    update

--- learning/app.js
+++ learning/app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-
-app.get('/',(req,res) => {
+app.get('/index',(req,res) => {
     res.render('top.ejs')
 });
 app.listen(3000);
