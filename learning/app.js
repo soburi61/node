5de8105f4d790df9973772002558668e56b6010d
soburi59 <591959ant@gmail.com>
2023-05-31 14:28:29 +0900

    add home.js

--- learning/app.js
+++ learning/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.get('/index',(req,res) => {
-    res.render('top.ejs')
+
+
+app.get('/',(req,res) => {
+    res.render('top.ejs');
 });
-app.listen(3000);
+console.log('10.133.90.88:3000/top')
+app.listen(3000,'10.133.90.88');
