d6cfcfb516443c91fdce6539948758390d29e069
soburi59 <591959ant@gmail.com>
2023-12-21 11:52:21 +0900

    update

--- SSK/public/js/add-class.js
+++ SSK/public/js/add-class.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
       const cell = $(this).parent(); // クリックされたセル（td）を取得
       const day = cell.data('day'); // 曜日を取得
       const time = cell.data('time'); // 時間を取得
-      console.log("1");
+      //console.log("1");
   
       // サーバーから科目一覧を取得
       $.getJSON('/getSubjects', function(subjects) {
@@ -21,15 +21,16 @@ $(document).ready(function() {
   
         // プルダウンメニューが選択されたら
         $('.subject-dropdown').on('change', function() {
-          console.log("1");
+          //console.log("1");
           const selectedSubjectId = $(this).val(); // 選択された科目のID
   
           // 選択された科目を時間割に追加するAPIを呼び出す
           $.post('/setClass', {subject_id: selectedSubjectId, day_of_week: day, time_slot: time}, function(response) {
             // 成功した場合の処理
-            console.log("1");
+            //console.log("1");
             if (response.success) {
               console.log('Subject added');
+              
               location.reload(); // Reload the page
             } else {
               console.log('Failed to add subject');
